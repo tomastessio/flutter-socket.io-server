497807368c6d6a9d38d0f4e204730a33783809c6
tomastessio <tomastessio@gmail.com>
2022-12-01 14:46:00 -0300

    Fin sección 3

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+
+// App de Express
+const app = express();
+
+
+
+// Node server
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+
+// Mensajes de Sockets
+io.on('connection', client => {
+    
+    console.log('Cliente conectado');
+
+    client.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
+
+    client.on('mensaje', (payload) => {
+        console.log('¡Mensaje!', payload);
+
+        io.emit('mensaje', {admin: 'Nuevo mensaje'});
+    });
+
+  });
+
+//Path pùblico
+const publicPath = path.resolve(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+
+
+
+server.listen(process.env.PORT, (err) => {
+
+    if (err) throw new Error(err);
+    
+    console.log(`Servidor corriendo en puerto:`, process.env.PORT);
+});
