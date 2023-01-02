a58624e24161db482cd03ca2fe6d60228aa393ff
tomastessio <tomastessio@gmail.com>
2023-01-02 10:07:33 -0300

    Cambios sobre bandas

--- index.js
+++ index.js
@@ -10,33 +10,14 @@ const app = express();
 
 // Node server
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket');
 
-
-// Mensajes de Sockets
-io.on('connection', client => {
-    
-    console.log('Cliente conectado');
-
-    client.on('disconnect', () => {
-        console.log('Cliente desconectado');
-    });
-
-    client.on('mensaje', (payload) => {
-        console.log('¡Mensaje!', payload);
-
-        io.emit('mensaje', {admin: 'Nuevo mensaje'});
-    });
-
-  });
 
 //Path pùblico
 const publicPath = path.resolve(__dirname, 'public');
 
 app.use(express.static(publicPath));
-
-
-
 
 server.listen(process.env.PORT, (err) => {
 
